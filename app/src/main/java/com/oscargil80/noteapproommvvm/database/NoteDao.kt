0ff3ef6d6ec8a8da78f56b1c2e62c1fa9299edbe
Oscar Medina <oscargil80@gmail.com>
2024-08-18 23:25:27 -0400

    Primer commit

---
+++ app/src/main/java/com/oscargil80/noteapproommvvm/database/NoteDao.kt
@@ -0,0 +1,27 @@
+package com.oscargil80.noteapproommvvm.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.oscargil80.noteapproommvvm.model.Note
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict =  OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM NOTES ORDER BY id DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Query ("SELECT * FROM NOTES WHERE noteTitle LIKE :query OR noteDesc like  :query")
+    fun searchNote(query:String?): LiveData<List<Note>>
+
+
+
+
+}
